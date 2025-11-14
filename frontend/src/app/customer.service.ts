3d4e6e08a9f018fac87cbb6a3943eba292d285b4
Namaru <54081611+Moises211@users.noreply.github.com>
2025-11-13 19:59:38 -0600

    Remove unused API_ROUTE from CustomerService
    Removed redundant API_ROUTE variable from CustomerService.

--- frontend/src/app/customer.service.ts
+++ frontend/src/app/customer.service.ts
@@ -9,9 +9,6 @@ import { environment } from './enviroment';
 })
 export class CustomerService {
   private apiUrl: string;
-  private API_ROUTE = '/api/customer';
-
-  constructor(private http: HttpClient, @Inject(PLATFORM_ID) private platformId: Object) {
 
   constructor(private http: HttpClient, @Inject(PLATFORM_ID) private platformId: Object) {
     this.apiUrl = isPlatformServer(this.platformId)
