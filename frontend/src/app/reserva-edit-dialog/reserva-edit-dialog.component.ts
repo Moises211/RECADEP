aefab2ccd93b702265d1e4518175343e10c6b919
unknown <moisesacosta8@gmail.com>
2025-11-27 22:48:43 -0600

    feat(reserva-usuario): anadimos un render yaml para iniciar el proceso.

---
+++ frontend/src/app/reserva-edit-dialog/reserva-edit-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reserva-edit-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './reserva-edit-dialog.component.html',
+  styleUrl: './reserva-edit-dialog.component.css'
+})
+export class ReservaEditDialogComponent {
+
+}
