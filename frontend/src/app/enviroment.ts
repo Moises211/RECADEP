a1cec076556f1d19fa40b050b7523463b760f8c7
Namaru <54081611+Moises211@users.noreply.github.com>
2025-11-29 13:26:12 -0600

    Fix formatting in environment.ts

--- frontend/src/app/enviroment.ts
+++ frontend/src/app/enviroment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   springLocal: 'http://localhost:8080/api',
   springDocker: 'http://spring-backend:8080/api',
-  springHostBridge: 'http://host.docker.internal:8080/api'
+  springHostBridge: 'http://host.docker.internal:8080/api',
   springRender: 'https://recadep-zox1.onrender.com/api'
 };
