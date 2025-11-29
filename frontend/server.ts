ba3885c1d932528ac4c2d5d117b2f81b3cd1e522
Namaru <54081611+Moises211@users.noreply.github.com>
2025-11-29 01:10:25 -0600

    Add logLevel option to proxy middleware
    Added logLevel option to proxy middleware for debugging.

--- frontend/server.ts
+++ frontend/server.ts
@@ -25,7 +25,8 @@ export function app(): express.Express {
   // Example Express Rest API endpoints
   server.use('/api', createProxyMiddleware({
   target: BACKEND_INTERNAL_URL,
-  changeOrigin: true
+  changeOrigin: true,
+  logLevel: 'debug'
   }));
 
   // Serve static files from /browser
