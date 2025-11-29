e1eccc67725b13425b3c247e2ffc6d09c080ec81
Namaru <54081611+Moises211@users.noreply.github.com>
2025-11-29 16:15:10 -0600

    Update enviroment.ts

--- frontend/src/app/enviroment.ts
+++ frontend/src/app/enviroment.ts
@@ -1,9 +1,7 @@
 export const environment = {
   production: false,
-  springRender: process.env['RENDER_BACKEND_URL'] 
-    ? `${process.env['RENDER_BACKEND_URL']}/api` // Si se inyecta, usa esa URL + /api
-    : 'https://recadep-zox1.onrender.com/api', // Fallback por defecto
   springLocal: 'http://localhost:8080/api',
   springDocker: 'http://spring-backend:8080/api',
-  springHostBridge: 'http://host.docker.internal:8080/api'  
+  springHostBridge: 'http://host.docker.internal:8080/api',
+  springRender: 'https://recadep-zox1.onrender.com/api'
 };
