ed9b688d9b6a96c552353d1aeeaea083d22d98a0
Namaru <54081611+Moises211@users.noreply.github.com>
2025-11-29 16:59:42 -0600

    Set production environment to true

--- frontend/src/app/enviroment.ts
+++ frontend/src/app/enviroment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  production: false,
+  production: true,
   springLocal: 'http://localhost:8080/api',
   springDocker: 'http://spring-backend:8080/api',
   springHostBridge: 'http://host.docker.internal:8080/api',
-  springRender: 'https://recadep-zox1.onrender.com/api'
+  springRender: '/api'
 };
