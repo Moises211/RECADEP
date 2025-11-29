f6f4e8ad928dda54f503e4079c8cae9bae56c365
Namaru <54081611+Moises211@users.noreply.github.com>
2025-11-29 13:57:38 -0600

    Refactor environment variable access in enviroment.ts
    Updated environment configuration to use bracket notation for accessing environment variables.

--- frontend/src/app/enviroment.ts
+++ frontend/src/app/enviroment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  production: process.env.NODE_ENV === 'production',
-  springRender: process.env.RENDER_BACKEND_URL 
-    ? `${process.env.RENDER_BACKEND_URL}/api` // Si se inyecta, usa esa URL + /api
+  production: process.env.NODE_ENV === 'production',  
+  springRender: process.env['RENDER_BACKEND_URL'] 
+    ? `${process.env['RENDER_BACKEND_URL']}/api` // Si se inyecta, usa esa URL + /api
     : 'https://recadep-zox1.onrender.com/api', // Fallback por defecto
   springLocal: 'http://localhost:8080/api',
   springDocker: 'http://spring-backend:8080/api',
