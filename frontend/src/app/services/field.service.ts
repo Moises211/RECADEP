8ed8a1e365be4b8a4b49d5b8ce6a33af0c40fd61
Namaru <54081611+Moises211@users.noreply.github.com>
2025-11-29 12:19:57 -0600

    Update FieldService to determine apiUrl dynamically
    Refactor FieldService to set apiUrl based on platform.

--- frontend/src/app/services/field.service.ts
+++ frontend/src/app/services/field.service.ts
@@ -8,9 +8,10 @@ import { environment } from '../enviroment';
   providedIn: 'root',
 })
 export class FieldService {
-  private apiUrl: string = '/api/field';
-  constructor(private http: HttpClient) { }
-  /*constructor(private http: HttpClient, @Inject(PLATFORM_ID) private platformId: Object) {
+  private apiUrl: string;
+  //private apiUrl: string = '/api/field';
+  //constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, @Inject(PLATFORM_ID) private platformId: Object) {
     this.apiUrl = isPlatformServer(this.platformId)
     ? 'http://spring-backend:8080/api/field'
     : 'https://recadep-zox1.onrender.com/api/field'
