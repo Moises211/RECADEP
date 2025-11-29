5a142121af408e6d34369de7f43b96d1730fb5fc
Namaru <54081611+Moises211@users.noreply.github.com>
2025-11-29 13:25:56 -0600

    Add springRender API endpoint to environment config

--- frontend/src/app/enviroment.ts
+++ frontend/src/app/enviroment.ts
@@ -3,4 +3,5 @@ export const environment = {
   springLocal: 'http://localhost:8080/api',
   springDocker: 'http://spring-backend:8080/api',
   springHostBridge: 'http://host.docker.internal:8080/api'
+  springRender: 'https://recadep-zox1.onrender.com/api'
 };
